310ae9483adb23c94088d9e279669472d372d6cf
LauraRuis <lauraruis@Live.nl>
2024-11-20 13:33:12 +0000

    Update linked.js

--- Demo/Scripts/linked.js
+++ Demo/Scripts/linked.js
@@ -11,7 +11,8 @@ window.onload = function() {
 
     // array of filenames that contain the data
     const filenames = [
-        "../DATA/answer_and_slope_pretraining_data_prepared_35B.json", "../DATA/answer_and_slope_pretraining_data_prepared_7B.json"];
+        "https://github.com/LauraRuis/lauraruis.github.io/blob/master/Demo/Data/answer_and_slope_pretraining_data_prepared_35B.json",
+        "https://github.com/LauraRuis/lauraruis.github.io/blob/master/Demo/Data/answer_and_slope_pretraining_data_prepared_7B.json"];
 
     var defaultColor = d3.rgb("#d4d4d4");
 
